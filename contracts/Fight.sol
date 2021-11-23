085881053ae4a94b57cb7072c73f99277fbf2b44
Sean Devlin <seanpdevlin@gmail.com>
2021-11-22 23:49:37 -0500

    add whitespace

--- contracts/Fight.sol
+++ contracts/Fight.sol
@@ -1,6 +1,7 @@
 // contracts/Market.sol
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.2;
+
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
